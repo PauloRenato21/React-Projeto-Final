58cfe0829a5a5067ea9b1475cfca17243127a8f5
PauloRenato21 <paulorenato401@gmail.com>
2021-07-16 16:43:39 -0300

    add new file atualizar responsavel

--- src/views/Responsavel/ResponsavelPut.js
+++ src/views/Responsavel/ResponsavelPut.js
@@ -4,7 +4,6 @@ import { makeStyles } from "@material-ui/core/styles";
 // core components
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
-import CustomInput from "components/CustomInput/CustomInput.js";
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardBody from "components/Card/CardBody.js";
@@ -58,63 +57,95 @@ const styles = {
     border: "1px solid #8e24aa",
     cursor: "pointer",
   },
+  label: {
+    display: "block",
+    marginTop: "24px",
+    font: "inherit",
+  },
+  input: {
+    border: "none",
+    borderBottom: "1px solid #d2d2d2",
+    width: "320px",
+    padding: "5px 0px",
+    fontSize: "1rem",
+  },
 };
 
 const useStyles = makeStyles(styles);
 
-export default function UserProfile() {
+export default function ResponsavelPut() {
   const classes = useStyles();
 
+  const [responsavelDados, setResponsavelDados] = useState({
+    id: "",
+    nome: "",
+    cpf: "",
+    grau_parentesco: "",
+    profissao: "",
+    local_trabalho: "",
+    telefone: "",
+    email: "",
+  });
+
+  const [responsavel, setResponsavel] = useState({
+    nome: "",
+    cpf: "",
+    grau_parentesco: "",
+    profissao: "",
+    local_trabalho: "",
+    telefone: "",
+    email: "",
+  });
+
   useEffect(() => {
-    console.log(atleta);
-    if (atleta.nome) {
+    console.log(responsavel);
+    if (responsavel.nome) {
       setDb();
     }
   });
 
-  const [atleta, setAtleta] = useState({
-    nome: "",
-    cpf: "",
-    endereco_rua: "",
-    endereco_bairro: "",
-    endereco_CEP: "",
-    naturalidade: "",
-    telefone: "",
-    dt_nascimento: "",
-    fk_turma_id: "",
-    fk_Responsavel_id: "",
-  });
+  useEffect(() => {
+    if (location.search.slice(1)) {
+      let id = location.search.slice(1);
+      api.get(`http://api.com/responsavel/${id}`).then((res) => {
+        const dadoResp = res.data;
+        dadoResp.map((count) =>
+          setResponsavelDados({
+            id: count.id,
+            nome: count.nome,
+            cpf: count.cpf,
+            grau_parentesco: count.grau_parentesco,
+            profissao: count.profissao,
+            local_trabalho: count.local_trabalho,
+            telefone: count.telefone,
+            email: count.email,
+          })
+        );
+      });
+    }
+  }, []);
 
   const setValueForm = () => {
-    let nomeAtleta = document.getElementById("nome").value;
-    let cpfAtleta = document.getElementById("cpf").value;
-    let ruaAtleta = document.getElementById("rua").value;
-    let bairroAtleta = document.getElementById("bairro").value;
-    let cepAtleta = document.getElementById("cep").value;
-    let naturalidadeAtleta = document.getElementById("naturalidade").value;
-    let telefoneAtleta = document.getElementById("telefone").value;
-    let dtNascimentoAtleta = document.getElementById("dtNascimento").value;
-    let fkTurma = document.getElementById("turma_id");
-    let fkResponsavel = document.getElementById("responsavel_id");
-    let valueFkTurma = fkTurma.options[fkTurma.selectedIndex].value;
-    let valueFkResponsavel =
-      fkResponsavel.options[fkResponsavel.selectedIndex].value;
-    setAtleta({
-      nome: nomeAtleta,
-      cpf: cpfAtleta,
-      endereco_rua: ruaAtleta,
-      endereco_bairro: bairroAtleta,
-      endereco_CEP: cepAtleta,
-      naturalidade: naturalidadeAtleta,
-      telefone: telefoneAtleta,
-      dt_nascimento: dtNascimentoAtleta,
-      fk_turma_id: valueFkTurma,
-      fk_Responsavel_id: valueFkResponsavel,
+    let nomeResp = document.getElementById("nome").value;
+    let cpfResp = document.getElementById("cpf").value;
+    let parentescoResp = document.getElementById("parentesco").value;
+    let trabalhoResp = document.getElementById("trabalho").value;
+    let profissaoResp = document.getElementById("profissao").value;
+    let telefoneResp = document.getElementById("telefone").value;
+    let emailResp = document.getElementById("email").value;
+    setResponsavel({
+      nome: nomeResp,
+      cpf: cpfResp,
+      grau_parentesco: parentescoResp,
+      local_trabalho: trabalhoResp,
+      profissao: profissaoResp,
+      telefone: telefoneResp,
+      email: emailResp,
     });
   };
 
   const setDb = () => {
-    api.post("http://api.com/atleta", atleta);
+    api.put(`http://api.com/responsavel/${responsavelDados.id}`, responsavel);
   };
 
   return (
@@ -123,228 +154,100 @@ export default function UserProfile() {
         <GridItem xs={12} sm={12} md={12}>
           <Card>
             <CardHeader color="primary">
-              <h4 className={classes.cardTitleWhite}>Cadastrar Atleta</h4>
+              <h4 className={classes.cardTitleWhite}>Atualizar Responsável</h4>
             </CardHeader>
-            {/* <form> */}
-            <CardBody>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Nome *"
-                    id="nome"
-                    formControlProps={{
-                      fullWidth: true,
-                    }}
-                    inputProps={{
-                      maxLength: 50,
-                      minLength: 4,
-                      required: true,
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="CPF *"
-                    id="cpf"
-                    formControlProps={{
-                      fullWidth: true,
-                    }}
-                    inputProps={{
-                      maxLength: 14,
-                      minLength: 14,
-                      placeholder: "000.000.000-00",
-                      pattern: "[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}",
-                      required: true,
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Endereço Rua *"
-                    id="rua"
-                    formControlProps={{
-                      fullWidth: true,
-                    }}
-                    inputProps={{
-                      maxLength: 50,
-                      required: true,
-                    }}
-                  />
-                </GridItem>
-              </GridContainer>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Endereço Nº *"
-                    id="numero"
-                    formControlProps={{
-                      fullWidth: true,
-                    }}
-                    inputProps={{
-                      maxLength: 5,
-                      required: true,
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Endereço Bairro *"
-                    id="bairro"
-                    formControlProps={{
-                      fullWidth: true,
-                    }}
-                    inputProps={{
-                      maxLength: 50,
-                      minLength: 3,
-                      required: true,
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Endereço CEP *"
-                    id="cep"
-                    formControlProps={{
-                      fullWidth: true,
-                    }}
-                    inputProps={{
-                      maxLength: 10,
-                      minLength: 8,
-                      placeholder: "0000-000",
-                      pattern: "[0-9]{4}-[0-9]{3}",
-                      required: true,
-                    }}
-                  />
-                </GridItem>
-              </GridContainer>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Naturalidade (País) *"
-                    id="naturalidade"
-                    formControlProps={{
-                      fullWidth: true,
-                    }}
-                    inputProps={{
-                      maxLength: 50,
-                      required: true,
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Problema Saúde"
-                    id="saude"
-                    formControlProps={{
-                      fullWidth: true,
-                    }}
-                    inputProps={{
-                      maxLength: 100,
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Alergia"
-                    id="alergia"
-                    formControlProps={{
-                      fullWidth: true,
-                    }}
-                    inputProps={{
-                      maxLength: 100,
-                    }}
-                  />
-                </GridItem>
-              </GridContainer>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Medicamento"
-                    id="medicamento"
-                    formControlProps={{
-                      fullWidth: true,
-                    }}
-                    inputProps={{
-                      maxLength: 100,
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Telefone *"
-                    id="telefone"
-                    formControlProps={{
-                      fullWidth: true,
-                    }}
-                    inputProps={{
-                      maxLength: 50,
-                      minLength: 7,
-                      placeholder: "(xx) xxxxx-xxxx",
-                      required: true,
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Email *"
-                    id="email"
-                    formControlProps={{
-                      fullWidth: true,
-                    }}
-                    inputProps={{
-                      maxLength: 100,
-                      placeholder: "@gmail.com",
-                      required: true,
-                    }}
-                  />
-                </GridItem>
-              </GridContainer>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={4}>
-                  <div className={classes.dtNascimento}>
-                    <label>Data de Nascimento *</label>
-                  </div>
-                  <input
-                    className={classes.inputNascimento}
-                    type="date"
-                    id="dtNascimento"
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <div className={classes.divSelect}>
-                    <label>Turma *</label>
-                  </div>
-                  <select
-                    className={classes.select}
-                    name="turma_id"
-                    id="turma_id"
-                  >
-                    <option></option>
-                    <option value="15">Turma 1</option>
-                  </select>
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  <div className={classes.divSelect}>
-                    <label>Responsável *</label>
-                  </div>
-                  <select
-                    className={classes.select}
-                    name="responsavel_id"
-                    id="responsavel_id"
-                  >
-                    <option></option>
-                    <option value="1">Responsável 1</option>
-                    <option value="1">Responsável 2</option>
-                  </select>
-                </GridItem>
-              </GridContainer>
-            </CardBody>
-            <CardFooter>
-              <button className={classes.button} onClick={() => setValueForm()}>
-                Enviar
-              </button>
-            </CardFooter>
-            {/* </form> */}
+            <form>
+              <CardBody>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <label className={classes.label}>Nome *</label>
+                    <input
+                      className={classes.input}
+                      id="nome"
+                      maxLength="100"
+                      minLength="4"
+                      required
+                      defaultValue={responsavelDados.nome}
+                    />
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <label className={classes.label}>CPF *</label>
+                    <input
+                      className={classes.input}
+                      id="cpf"
+                      maxLength="14"
+                      minLength="14"
+                      placeholder="000.000.000-00"
+                      pattern="[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}"
+                      required
+                      defaultValue={responsavelDados.cpf}
+                    />
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <label className={classes.label}>Grau Parentesco *</label>
+                    <input
+                      className={classes.input}
+                      id="parentesco"
+                      maxLength="100"
+                      required
+                      defaultValue={responsavelDados.grau_parentesco}
+                    />
+                  </GridItem>
+                </GridContainer>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <label className={classes.label}>Profissão *</label>
+                    <input
+                      className={classes.input}
+                      id="profissao"
+                      maxLength="50"
+                      required
+                      defaultValue={responsavelDados.profissao}
+                    />
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <label className={classes.label}>Local de Trabalho *</label>
+                    <input
+                      className={classes.input}
+                      id="trabalho"
+                      maxLength="100"
+                      required
+                      defaultValue={responsavelDados.local_trabalho}
+                    />
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <label className={classes.label}>Telefone *</label>
+                    <input
+                      className={classes.input}
+                      id="telefone"
+                      maxLength="50"
+                      required
+                      defaultValue={responsavelDados.telefone}
+                    />
+                  </GridItem>
+                </GridContainer>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <label className={classes.label}>Email *</label>
+                    <input
+                      className={classes.input}
+                      id="email"
+                      maxLength="100"
+                      required
+                      defaultValue={responsavelDados.email}
+                    />
+                  </GridItem>
+                </GridContainer>
+              </CardBody>
+              <CardFooter>
+                <button
+                  className={classes.button}
+                  onClick={() => setValueForm()}
+                >
+                  Enviar
+                </button>
+              </CardFooter>
+            </form>
           </Card>
         </GridItem>
       </GridContainer>
